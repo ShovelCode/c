f32bb1805cef9f7478a5b266f23af2b895607a69
Christopher Ray <vde435@gmail.com>
2023-07-03 19:02:18 -0500

    Create function-signature.c

---
+++ function-signature.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// Function declaration
+void greet();
+
+int main() {
+    // Call the greet function
+    greet();
+
+    // Additional code for the driver program
+
+    return 0;
+}
+
+// Function definition
+void greet() {
+    printf("Hello, world!\n");
+}
